b23354da6ef55393a01328b28414eed58c7dda83
ulfahumaira <72180111+ulfahumaira@users.noreply.github.com>
2021-01-01 16:06:11 +0700

    Update Denda.sql

--- Insert Data/Denda.sql
+++ Insert Data/Denda.sql
@@ -1,5 +1,5 @@
-INSERT INTO denda (ID_Siswa,ID_Peminjam,Biaya_Denda)
-SELECT ID_Siswa,ID, DATEDIFF( Tanggal_Pengembalian, Batas_Pengembalian)*1000
+INSERT INTO denda (ID_Siswa,ID_Peminjam,Biaya_Denda,Bayar)
+SELECT ID_Siswa,ID, DATEDIFF( Tanggal_Pengembalian, Batas_Pengembalian)*1000,(select case when rand() < 0.5 then 1 else 0 end))
 FROM peminjam_buku
 WHERE denda=1;
 commit;
